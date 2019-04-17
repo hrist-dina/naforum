01abede0718da067c09cfc62af959af24f8cc08e
Dina Hrist <hdina@inbox.ru>
2019-04-17 23:08:13 +0700

    Added slider-expert

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,20 +1,25 @@
-$(document).ready(function() {
-    var slickSlider = $(".js-slider-expert");
-    slickSlider.slick({
-        arrows: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-        //prevArrow:
-        //'<div class="slider-brand__arrow-left"><i class="fas fa-angle-left"></i></div>',
-        //nextArrow:
-        //'<div class="slider-brand__arrow-right"><i class="fas fa-angle-right"></i></div>'
-        /*responsive: [
-                {
-                  settings: {
-                    slidesToShow: 1,
-                    arrows: false,
-                  }
-                }
-            ],*/
-    });
+$(document).ready(function () {
+  var slickSlider = $(".js-slider-expert");
+  slickSlider.slick({
+    arrows: false,
+    infinite: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    //centerMode: true,
+    //centerPadding: '15px',
+    //prevArrow:
+    //'<div class="slider-brand__arrow-left"><i class="fas fa-angle-left"></i></div>',
+    //nextArrow:
+    //'<div class="slider-brand__arrow-right"><i class="fas fa-angle-right"></i></div>'
+    /*responsive: [
+            {
+              settings: {
+                slidesToShow: 1,
+                arrows: false,
+              }
+            }
+        ],*/
+  });
 });
