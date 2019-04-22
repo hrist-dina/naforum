e5cb308b47e92b9cb7d2ece630cce34e4dfea5fa
Dina Hrist <hdina@inbox.ru>
2019-04-23 00:43:26 +0700

    Added location, organizer page

--- dev/js/script.js
+++ dev/js/script.js
@@ -43,6 +43,57 @@ $(document).ready(function() {
             className: "map-balloon",
             iconSize: [52, 53]
         });
-        DG.marker([54.98, 82.89], { icon: mapIcon }).addTo(map);
+        DG.marker([54.98, 82.89], {icon: mapIcon}).addTo(map);
     });
+
+    if ($("#js-map").length) {
+        ymaps.ready(init);
+    }
+
+    function init() {
+        var map = new ymaps.Map(
+            // ID DOM-элемента, в который будет добавлена карта.
+            "js-map",
+            // Параметры карты.
+            {
+                // Географические координаты центра отображаемой карты.
+                center: [54.98, 82.89],
+                // Масштаб.
+                zoom: 3,
+                // Тип покрытия карты: "Спутник".
+                type: "",
+                // Элементы управления
+                controls: ["geolocationControl", "zoomControl"]
+            },
+            {
+                // Поиск по организациям.
+                //searchControlProvider: 'yandex#search'
+            }
+        );
+
+        map.behaviors.disable("scrollZoom");
+
+        myPlacemark = new ymaps.Placemark(
+            [54.98, 82.89],
+            {
+                hintContent: "Собственный значок метки с контентом",
+                balloonContent: "А эта — новогодняя",
+                iconContent: "12"
+            },
+            {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: "default#image",
+                // Своё изображение иконки метки.
+                iconImageHref: "/img/balloon.svg",
+                // Размеры метки.
+                iconImageSize: [52, 53],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-5, -38]
+            }
+        );
+
+        map.geoObjects.add(myPlacemark);
+    }
 });
